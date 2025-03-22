1128f55d2a877f7395291c6104339d2f4b20e48d
Anuraag Pandhi <anumon6395@gmail.com>
2025-03-21 23:49:49 -0400

    implemented paddle

--- Paddle.py
+++ Paddle.py
@@ -1,1 +1,14 @@
-# Paddle size, movement
+class Paddle:
+    
+    def __init__(self, initial_position, length):
+        self.x, self.y = initial_position
+        self.length = length
+
+    def update_position(self, direction, step):
+        self.y += direction * step
+    
+    def get_posiiton(self):
+        return (self.x, self.y)
+    
+    def set_position(self, initial_position):
+        self.x, self.y = initial_position
